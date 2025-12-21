4d762f14982fc09213e834376b2264451719e94c
bala-amypo <bala@amypo.in>
2025-12-21 13:59:52 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,11 +1,13 @@
 package com.example.demo.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import java.util.List;
 
 @Configuration
@@ -13,16 +15,16 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
-
-        Server portalServer = new Server()
-                .url("https://9534.pro604cr.amypo.ai")
-                .description("Portal Gateway Server");
-
+        final String securitySchemeName = "Bearer Authentication";
+        
         return new OpenAPI()
                 .info(new Info()
                         .title("Digital Asset Lifecycle & Audit Trail API")
                         .version("1.0")
                         .description("API for managing IT assets with full audit trails."))
-                .servers(List.of(portalServer));
+                // Set your specific server URL
+                .servers(List.of(
+                        .url("https://9534.pro604cr.amypo.ai")
+                ));
     }
 }
