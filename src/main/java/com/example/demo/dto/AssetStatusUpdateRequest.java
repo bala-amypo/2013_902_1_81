d8927fc0fa0bdf754254a28d1616edeaad53c097
bala-amypo <bala@amypo.in>
2025-12-26 14:17:32 +0000

    User commit

--- src/main/java/com/example/demo/dto/AssetStatusUpdateRequest.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.dto;
-
-public class AssetStatusUpdateRequest {
-    private String status;
-
-    // Getters and Setters
-    public String getStatus() { return status; }
-    public void setStatus(String status) { this.status = status; }
-}
